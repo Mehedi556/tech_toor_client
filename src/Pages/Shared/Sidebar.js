321fc21bba07c46906fdbbbe01de21977bd9aed4
mehedi556 <mehe556di@gmail.com>
2022-10-27 23:30:23 +0600

    final commit

--- src/Pages/Shared/Sidebar.js
+++ src/Pages/Shared/Sidebar.js
@@ -7,61 +7,70 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider';
 
 const Sidebar = () => {
+  const { googleLogin, githubLogin, setUser } = useContext(AuthContext);
+  const [options, setOptions] = useState([]);
 
-    const {googleLogin , githubLogin , setUser} = useContext(AuthContext);
-    const [options , setOptions] = useState([]);
+  useEffect(() => {
+    fetch('https://tech-toor-server.vercel.app/categories')
+      .then(res => res.json())
+      .then(data => setOptions(data));
+  }, []);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/categories')
-        .then(res => res.json())
-        .then(data => setOptions(data))
-    } , [])
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
-    const googleProvider = new GoogleAuthProvider();
-    const githubProvider = new GithubAuthProvider();
+  const handleGoogle = () => {
+    googleLogin(googleProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => console.error(error));
+  };
 
-    const handleGoogle = () => {
-        googleLogin(googleProvider)
-        .then(result => {
-            const user = result.user;
-            setUser(user);
-            console.log(user);
-        }).catch(error => console.error(error))
-    }
+  const handleGithub = () => {
+    githubLogin(githubProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => console.error(error));
+  };
 
-    const handleGithub = () => {
-        githubLogin(githubProvider)
-        .then(result => {
-            const user = result.user;
-            setUser(user);
-            console.log(user);
-        }).catch(error => console.error(error))
-    }
-    
-
-    return (
-        <div className=''>
-            <h2 className='text-center font-bold text-2xl mt-9'>All Courses</h2>
-            {
-                options.map(option => <div className=' text-center'>
-                    <Link to={`/category/${option.id}`}><button  key={option.id} className="btn btn-wide btn-outline my-2">{option.name}</button></Link>
-
-                    
-                </div>)
-            }
-
-            <div className='gap-2 mt-12'>
-
-                <button onClick={handleGoogle} className='btn btn-outline mx-auto block text-xl mb-2'>Signin  by Google</button>
-
-                <button onClick={handleGithub} className='btn btn-outline mx-auto block text-xl'>Signin  by Github</button>
-                
-                <button></button>
-            </div>
-
-        
+  return (
+    <div className="">
+      <h2 className="text-center font-bold text-2xl mt-9">All Courses</h2>
+      {options.map(option => (
+        <div className=" text-center">
+          <Link to={`/category/${option.id}`}>
+            <button key={option.id} className="btn btn-wide btn-outline my-2">
+              {option.name}
+            </button>
+          </Link>
         </div>
-    );
+      ))}
+
+      <div className="gap-2 mt-12">
+        <button
+          onClick={handleGoogle}
+          className="btn btn-outline mx-auto block text-xl mb-2"
+        >
+          Signin by Google
+        </button>
+
+        <button
+          onClick={handleGithub}
+          className="btn btn-outline mx-auto block text-xl"
+        >
+          Signin by Github
+        </button>
+
+        <button></button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,62 +1,66 @@
-import { createBrowserRouter } from "react-router-dom";
-import Main from "../Layout/Main";
-import Blog from "../Pages/Blog/Blog";
-import Category from "../Pages/Category/Category";
-import Error from "../Pages/Error/Error";
-import Faq from "../Pages/Faq/Faq";
-import Home from "../Pages/Home/Home";
-import Login from "../Pages/Login/Login";
-import Premium from "../Pages/Premium/Premium";
-import Register from "../Pages/Register/Register";
-import PrivetRoute from "./PrivetRoute/PrivetRoute";
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Blog from '../Pages/Blog/Blog';
+import Category from '../Pages/Category/Category';
+import Error from '../Pages/Error/Error';
+import Faq from '../Pages/Faq/Faq';
+import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
+import Premium from '../Pages/Premium/Premium';
+import Register from '../Pages/Register/Register';
+import PrivetRoute from './PrivetRoute/PrivetRoute';
 
 export const routes = createBrowserRouter([
-    {
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
         path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/details'),
-                // loader: ({params}) => fetch(`http://localhost:5000/details/${params.id}`)
-            },
-            {
-                path: '/login',
-                element: <Login></Login>
-            },
-            {
-                path: '/register',
-                element: <Register></Register>
-            },
-            {
-                path: '/blog',
-                element: <Blog></Blog>
-            },
-            {
-                path: '/faq',
-                element: <Faq></Faq>
-            },
-            
-            {
-                path: '/premium',
-                element: <PrivetRoute><Premium></Premium></PrivetRoute>
-            },
-            {
-                path: '/category/:id',
-                element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/details/${params.id}`)
-            },
-            
-            {
-                path: '*',
-                element: <Error></Error>
-            }
-            
-        ]
-    },
-    {
+        element: <Home></Home>,
+        loader: () => fetch('https://tech-toor-server.vercel.app/details'),
+        // loader: ({params}) => fetch(`https://tech-toor-server.vercel.app/details/${params.id}`)
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
+      },
+      {
+        path: '/register',
+        element: <Register></Register>,
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>,
+      },
+      {
+        path: '/faq',
+        element: <Faq></Faq>,
+      },
+
+      {
+        path: '/premium',
+        element: (
+          <PrivetRoute>
+            <Premium></Premium>
+          </PrivetRoute>
+        ),
+      },
+      {
+        path: '/category/:id',
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(`https://tech-toor-server.vercel.app/details/${params.id}`),
+      },
+
+      {
         path: '*',
-        element: <Error></Error>
-    }
-])
+        element: <Error></Error>,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <Error></Error>,
+  },
+]);
